5046d6e1aaf570a719943005c272efa64ce153c5
Sabuj <sabujlovescoding@gmail.com>
2019-09-28 13:48:36 +0530

    extracted stats page

--- vir.py
+++ vir.py
@@ -22,6 +22,13 @@ if(page.status_code != 200):
 # print(page.text)
 # Note- page.text contains the html or the page source
 soup = BeautifulSoup(page.text, 'html.parser')
-print(soup.prettify())
+# print(soup.prettify())
+
+#First extract the stats of the contest from the standings page
+page_stats = requests.get(contest_url + '/standings')
+if(page_stats.status_code != 200):
+    print("Failed to retrive stats for contest {}!!!!".format(contest_id))
+soup_stats = BeautifulSoup(page_stats.text, 'html.parser')
+print(soup_stats.prettify())
 
 
